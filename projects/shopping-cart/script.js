3a9afc231c7abc650eafb6fbd3122b17f94cbfc2
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-06-30 00:11:19 -0400

    Atualização do projeto via script em qua 30 jun 2021 00:11:19 -04

--- projects/shopping-cart/script.js
+++ projects/shopping-cart/script.js
@@ -1,4 +1,6 @@
-window.onload = function onload() { };
+window.onload = function onload() { 
+  
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -26,19 +28,27 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+const test = createProductItemElement({ id: 123, name: 'pc', image: 'http://http2.mlstatic.com/D_817225-MLB45400911410_032021-O.jpg' });
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
+// console.log(test);
+
+const sectionMain = document.querySelector('.items');
+
+sectionMain.appendChild(test);
+
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
   
-}
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
